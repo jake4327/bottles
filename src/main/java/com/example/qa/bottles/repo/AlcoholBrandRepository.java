2550934c788bfdcafb61500f5276531a185c735e
Jake Stone <jakestone4327@gmail.com>
2020-08-31 11:31:53 +0100

    repository decorator added to each repo interface

--- src/main/java/com/example/qa/bottles/repo/AlcoholBrandRepository.java
+++ src/main/java/com/example/qa/bottles/repo/AlcoholBrandRepository.java
@@ -2,6 +2,8 @@ package com.example.qa.bottles.repo;
 
 import com.example.qa.bottles.domain.AlcoholBrand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlcoholBrandRepository extends JpaRepository<AlcoholBrand, Long> {
 }
--- src/main/java/com/example/qa/bottles/repo/BarRepository.java
+++ src/main/java/com/example/qa/bottles/repo/BarRepository.java
@@ -2,6 +2,8 @@ package com.example.qa.bottles.repo;
 
 import com.example.qa.bottles.domain.Bar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BarRepository extends JpaRepository<Bar, Long> {
 }
