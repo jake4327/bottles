cf7986767fa6d6ad29075cc80d7447e1f1338dc9
Jake Stone <jakestone4327@gmail.com>
2020-09-02 13:08:19 +0100

    AlcoholBrandService was changed so that it uses DTO's instead of nesting the loop

--- src/main/java/com/example/qa/bottles/service/AlcoholBrandService.java
+++ src/main/java/com/example/qa/bottles/service/AlcoholBrandService.java
@@ -1,40 +1,49 @@
 package com.example.qa.bottles.service;
 
 import com.example.qa.bottles.domain.AlcoholBrand;
+import com.example.qa.bottles.dto.AlcoholBrandDTO;
 import com.example.qa.bottles.exceptions.AlcoholBrandNotFoundException;
 import com.example.qa.bottles.repo.AlcoholBrandRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AlcoholBrandService {
 
     private final AlcoholBrandRepository repo;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public AlcoholBrandService(AlcoholBrandRepository repo) {
+    public AlcoholBrandService(AlcoholBrandRepository repo, ModelMapper modelMapper) {
         this.repo = repo;
+        this.modelMapper = modelMapper;
     }
 
-    public List<AlcoholBrand> readAllAlcoholBrands(){
-        return this.repo.findAll();
+    private AlcoholBrandDTO mapToDTO(AlcoholBrand alcoholBrand){
+        return this.modelMapper.map(alcoholBrand, AlcoholBrandDTO.class);
     }
 
-    public AlcoholBrand createAlcoholBrand(AlcoholBrand alcoholBrand){
-        return this.repo.save(alcoholBrand);
+    public List<AlcoholBrandDTO> readAllAlcoholBrands(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public AlcoholBrand findAlcoholBrandById(Long id){
-        return this.repo.findById(id).orElseThrow(AlcoholBrandNotFoundException::new);
+    public AlcoholBrandDTO createAlcoholBrand(AlcoholBrand alcoholBrand){
+        return this.mapToDTO(repo.save(alcoholBrand));
+    }
+
+    public AlcoholBrandDTO findAlcoholBrandById(Long id){
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(AlcoholBrandNotFoundException::new));
 
     }
 
-    public AlcoholBrand updateAlcoholBrand(Long id, AlcoholBrand alcoholBrand){
-        AlcoholBrand update = findAlcoholBrandById(id);
+    public AlcoholBrandDTO updateAlcoholBrand(Long id, AlcoholBrand alcoholBrand){
+        AlcoholBrand update = this.repo.findById(id).orElseThrow(AlcoholBrandNotFoundException::new);
         update.setName(alcoholBrand.getName());
-        return this.repo.save(update);
+        return this.mapToDTO(repo.save(update));
     }
 
     public Boolean deleteAlcoholBrandById(Long id){
